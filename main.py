0220efc047af6e2c4e05bd925214d80e8bc1e353
Daliana-Solis <dsolisso@uci.edu>
2023-09-22 15:47:25 -0500

    Connected Webcam

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2
+import time
+
+video = cv2.VideoCapture(0)
+time.sleep(1)
+
+while True:
+    check, frame = video.read()
+    cv2.imshow("my video", frame)
+
+    key = cv2.waitKey(1)
+
+    # if user clicks 'q', webcam closes/ends
+    if key == ord('q'):
+        break
+
+
+video.release()
+
+
+
