c3efc65a60884397d52bbd764e1b3b7ae6a427be
Daliana-Solis <dsolisso@uci.edu>
2023-09-22 15:55:29 -0500

    Changed frame to gray scale

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ time.sleep(1)
 
 while True:
     check, frame = video.read()
-    cv2.imshow("my video", frame)
+    #Change frame to gray
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #blurness
+    gray_frame_gua = cv2.GaussianBlur(gray_frame, (21,21), 0)
+    cv2.imshow("my video", gray_frame)
 
     key = cv2.waitKey(1)
 
